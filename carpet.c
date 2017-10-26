e6547a3de7465b03a8f50bcadddc87b8c6aa4c69
outfrost <kotlet.bahn@gmail.com>
2017-10-26 21:50:13 +0200

    More work on Sierpinski's Carpet render

--- carpet.c
+++ carpet.c
@@ -3,13 +3,17 @@
 #include <stdbool.h>
 #include <time.h>
 
-typedef float point2f[2];
+//typedef float point2f[2];
+typedef struct {
+	float x;
+	float y;
+} point2f;
 
 float randf() {
 	return (float)rand() / (float)RAND_MAX;
 }
 
-/*bool* carpet(int iterations) {
+bool* carpet(int iterations) {
 	int size = 1;
 	for (int i = iterations; i > 0; --i)
 		size *= 3;
@@ -18,31 +22,42 @@ float randf() {
 	for (int i = 0; i < size * size; ++i)
 		*(points+i) = true;
 	
-	int square_size = size;
-	for (; iterations > 0; --iterations) {
+	for (int square_size = size; iterations > 0; --iterations) {
 		square_size /= 3;
-		int x = size - 1;
-		while (x >= 0) {
+		for (int x = size - 1; x >= 0; --x) {
 			int y = size - 1;
-			while (y >= 0) {
-				if 
+			for (int y = size - 1; y >= 0; --y) {
+				if ((x / square_size) % 3 == 1 && (y / square_size) % 3 == 1)
+					points[x * size + y] = false;
+			}
+		}
 	}
-}*/
+}
+
+void draw_square(point2f point, float size) {
+	float halfsize = size / 2;
+	float deviation = size / 4;
+	
+	glBegin(GL_POLYGON);
+	glColor3f(randf(), randf(), randf());
+	glVertex2f(point.x - halfsize + randf() * deviation, point.y - halfsize + randf() * deviation);
+	glColor3f(randf(), randf(), randf());
+	glVertex2f(point.x + halfsize + randf() * deviation, point.y - halfsize + randf() * deviation);
+	glColor3f(randf(), randf(), randf());
+	glVertex2f(point.x + halfsize + randf() * deviation, point.y + halfsize + randf() * deviation);
+	glColor3f(randf(), randf(), randf());
+	glVertex2f(point.x - halfsize + randf() * deviation, point.y + halfsize + randf() * deviation);
+	glEnd();
+}
 
 void render_scene() {
 	// Clear the stage using the current clear colour
 	glClear(GL_COLOR_BUFFER_BIT);
 	
-	glBegin(GL_POLYGON);
-	glColor3f(randf(), randf(), randf());
-	glVertex2f(-50.0f + randf()*5.0f, 50.0f + randf()*5.0f);
-	glColor3f(randf(), randf(), randf());
-	glVertex2f(50.0f + randf()*5.0f, 50.0f + randf()*5.0f);
-	glColor3f(randf(), randf(), randf());
-	glVertex2f(50.0f + randf()*5.0f, -50.0f + randf()*5.0f);
-	glColor3f(randf(), randf(), randf());
-	glVertex2f(-50.0f + randf()*5.0f, -50.0f + randf()*5.0f);
-	glEnd();
+	int iterations = 1
+	bool * points = carpet(iterations);
+	
+	for (int x = 0; x <  ; ++x)
 	
 	// Flush draw calls to execution
 	glFlush();
