bc5f14c523220ae97b9cf1bf08b1540bdbf07afe
outfrost <kotlet.bahn@gmail.com>
2017-11-20 12:12:25 +0100

    5th lab excercise
    Hacked together some dynamic lighting and normal mapping

---
+++ egg.c
@@ -0,0 +1,227 @@
+//#include <GL/glut.h>
+//#include <math.h>
+
+point3f * egg_vertices = NULL;
+point3f * vertex_colors = NULL;
+point3f * egg_normals = NULL;
+
+point3f * get_vertex(int i, int k) {
+	return &(egg_vertices[i * EGG_SUBDIVISIONS + k]);
+}
+
+point3f * get_vertex_color(point3f * vertex) {
+	return vertex_colors + (vertex - egg_vertices);
+}
+
+point3f * get_vertex_normal(point3f * vertex) {
+	return egg_normals + (vertex - egg_vertices);
+}
+
+float sgnf(float x) {
+	if (x > 0)
+		return 1.0f;
+	else if (x < 0)
+		return -1.0f;
+	else
+		return 0.0f;
+}
+
+void create_vertex(float u, float v, point3f * result, point3f * normal) {
+	(*result)[0] = (-  90.0f * powf(u, 5.0f)
+			+ 225.0f * powf(u, 4.0f)
+			- 270.0f * powf(u, 3.0f)
+			+ 180.0f * powf(u, 2.0f)
+			-  45.0f * u) * cosf (M_PI * v);
+	
+	(*result)[1] =    160.0f * powf(u, 4.0f)
+			- 320.0f * powf(u, 3.0f)
+			+ 160.0f * powf(u, 2.0f)
+			- 5.0f;
+	
+	(*result)[2] = (-  90.0f * powf(u, 5.0f)
+			+ 225.0f * powf(u, 4.0f)
+			- 270.0f * powf(u, 3.0f)
+			+ 180.0f * powf(u, 2.0f)
+			-  45.0f * u) * sinf(M_PI * v);
+	
+	float xu = (- 450.0f * powf(u, 4.0f)
+		    + 900.0f * powf(u, 3.0f)
+		    - 810.0f * powf(u, 2.0f)
+		    + 360.0f * u
+		    -  45.0f) * cosf(M_PI * v);
+	
+	float xv = (   90.0f * powf(u, 5.0f)
+		    - 225.0f * powf(u, 4.0f)
+		    + 270.0f * powf(u, 3.0f)
+		    - 180.0f * powf(u, 2.0f)
+		    +  45.0f * u) * M_PI * sinf(M_PI * v);
+	
+	float yu =   640.0f * powf(u, 3.0f)
+		   - 960.0f * powf(u, 2.0f)
+		   + 320.0f * u;
+	
+	float yv = 0.0f;
+	
+	float zu = (- 450.0f * powf(u, 4.0f)
+		    + 900.0f * powf(u, 3.0f)
+		    - 810.0f * powf(u, 2.0f)
+		    + 360.0f * u
+		    -  45.0f) * sinf(M_PI * v);
+	
+	float zv = (   90.0f * powf(u, 5.0f)
+		    - 225.0f * powf(u, 4.0f)
+		    + 270.0f * powf(u, 3.0f)
+		    - 180.0f * powf(u, 2.0f)
+		    +  45.0f * u) * (- M_PI) * cosf(M_PI * v);
+	
+	(*normal)[0] = yu * zv - zu * yv;
+	(*normal)[1] = zu * xv - xu * zv;
+	(*normal)[2] = xu * yv - yu * xv;
+	if (u > 0.5f) {
+		(*normal)[0] *= -1.0f;
+		(*normal)[1] *= -1.0f;
+		(*normal)[2] *= -1.0f;
+	}
+/*	(*normal)[0] *= -sgnf((*normal)[2]);
+	(*normal)[1] *= -sgnf((*normal)[2]);
+	(*normal)[2] *= -sgnf((*normal)[2]);
+*/}
+
+void create_vertex_color(point3f * vertex, point3f * result) {
+	for (int i = 0; i < 3; ++i) {
+		(*result)[i] = ((*vertex)[i] + 5.0f) / 10.0f;
+	}
+}
+
+void compute_egg_vertices() {
+	egg_vertices = realloc(egg_vertices,
+			       EGG_SUBDIVISIONS * EGG_SUBDIVISIONS
+					* sizeof(point3f));
+	vertex_colors = realloc(vertex_colors,
+				EGG_SUBDIVISIONS * EGG_SUBDIVISIONS
+					* sizeof(point3f));
+	egg_normals = realloc(egg_normals,
+			      EGG_SUBDIVISIONS * EGG_SUBDIVISIONS
+					* sizeof(point3f));
+	
+	for (int i = 0; i < EGG_SUBDIVISIONS; ++i) {
+		float u = (1.0f / EGG_SUBDIVISIONS) * i;
+		for (int k = 0; k < EGG_SUBDIVISIONS; ++k) {
+			float v = (1.0f / EGG_SUBDIVISIONS) * k;
+			point3f * vertex = get_vertex(i, k);
+			point3f * normal = get_vertex_normal(vertex);
+			create_vertex(u, v, vertex, normal);
+			create_vertex_color(vertex, get_vertex_color(vertex));
+		}
+	}
+}
+
+void vert(point3f * vertex) {
+	memcpy(mat_ambient, get_vertex_color(vertex), sizeof(point3f));
+	memcpy(mat_diffuse, get_vertex_color(vertex), sizeof(point3f));
+	memcpy(mat_specular, get_vertex_color(vertex), sizeof(point3f));
+	glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialf(GL_FRONT, GL_SHININESS, mat_shininess);
+	
+	point3f * normal = get_vertex_normal(vertex);
+	glNormal3f((*normal)[0], (*normal)[1], (*normal)[2]);
+	glVertex3f((*vertex)[0], (*vertex)[1], (*vertex)[2]);
+}
+
+void draw_egg_vertices() {
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glBegin(GL_POINTS);
+	for (int i = 0; i < EGG_SUBDIVISIONS; ++i) {
+		for (int k = 0; k < EGG_SUBDIVISIONS; ++k) {
+			point3f * vertex = get_vertex(i, k);
+			glVertex3f((*vertex)[0], (*vertex)[1], (*vertex)[2]);
+		}
+	}
+	glEnd();
+}
+
+void edge(point3f * vertex, point3f * next_vertex) {
+//debug	printf("%ld %ld, %ld %ld\n", (vertex - egg_vertices)/EGG_SUBDIVISIONS, (vertex - egg_vertices)%EGG_SUBDIVISIONS, (next_vertex - egg_vertices)/EGG_SUBDIVISIONS, (next_vertex - egg_vertices)%EGG_SUBDIVISIONS);
+//	glVertex3f((*vertex)[0], (*vertex)[1], (*vertex)[2]);
+//	glVertex3f((*next_vertex)[0], (*next_vertex)[1], (*next_vertex)[2]);
+	vert(vertex);
+	vert(next_vertex);
+}
+
+void face(point3f * v1, point3f * v2, point3f * v3) {
+/*	point3f * v_color = get_vertex_color(v1);
+	glColor3f((*v_color)[0], (*v_color)[1], (*v_color)[2]);
+	glVertex3f((*v1)[0], (*v1)[1], (*v1)[2]);
+	v_color = get_vertex_color(v2);
+	glColor3f((*v_color)[0], (*v_color)[1], (*v_color)[2]);
+	glVertex3f((*v2)[0], (*v2)[1], (*v2)[2]);
+	v_color = get_vertex_color(v3);
+	glColor3f((*v_color)[0], (*v_color)[1], (*v_color)[2]);
+	glVertex3f((*v3)[0], (*v3)[1], (*v3)[2]);
+*/	vert(v1);
+	vert(v2);
+	vert(v3);
+}
+
+/*void edge_n(point3f * vertex, point3f * normal) {
+	memcpy(mat_ambient, get_vertex_color(vertex), sizeof(point3f));
+	memcpy(mat_diffuse, get_vertex_color(vertex), sizeof(point3f));
+	memcpy(mat_specular, get_vertex_color(vertex), sizeof(point3f));
+	glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialf(GL_FRONT, GL_SHININESS, mat_shininess);
+	
+	glVertex3f((*vertex)[0], (*vertex)[1], (*vertex)[2]);
+	glVertex3f((*vertex)[0] + (*normal)[0],
+		   (*vertex)[1] + (*normal)[1],
+		   (*vertex)[2] + (*normal)[2]);
+}*/
+
+void draw_egg_edges() {
+	glColor3f(1.0f, 1.0f, 0.5f);
+	glBegin(GL_LINES);
+	for (int i = 0, i_next = 1; i < EGG_SUBDIVISIONS; ++i, ++i_next) {
+		if (i_next == EGG_SUBDIVISIONS)
+			i_next = 0;
+		for (int k = 0, k_next = 1; k < EGG_SUBDIVISIONS; ++k, ++k_next) {
+			point3f * vertex = get_vertex(i, k);
+//			point3f * normal = get_vertex_normal(vertex);
+//			edge_n(vertex, normal);
+			edge(vertex, get_vertex(i_next, k));
+			if (k_next == EGG_SUBDIVISIONS) {
+				edge(vertex, get_vertex(EGG_SUBDIVISIONS - i, 0));
+				edge(vertex, get_vertex(EGG_SUBDIVISIONS - i_next, 0));
+			}
+			else {
+				edge(vertex, get_vertex(i, k_next));
+				edge(vertex, get_vertex(i_next, k_next));
+			}
+		}
+	}
+	glEnd();
+}
+
+void draw_egg_faces() {
+	glColor3f(0.0f, 0.6f, 0.4f);
+	glBegin(GL_TRIANGLES);
+	for (int i = 0, i_next = 1; i < EGG_SUBDIVISIONS; ++i, ++i_next) {
+		if (i_next == EGG_SUBDIVISIONS)
+			i_next = 0;
+		for (int k = 0, k_next = 1; k < EGG_SUBDIVISIONS; ++k, ++k_next) {
+			point3f * v00 = get_vertex(i, k);
+			point3f * v01 = k_next == EGG_SUBDIVISIONS ?
+					  get_vertex(EGG_SUBDIVISIONS - i, 0)
+					: get_vertex(i, k_next);
+			point3f * v10 = get_vertex(i_next, k);
+			point3f * v11 = k_next == EGG_SUBDIVISIONS ?
+					  get_vertex(EGG_SUBDIVISIONS - i_next, 0)
+					: get_vertex(i_next, k_next);
+			face(v00, v01, v11);
+			face(v00, v10, v11);
+		}
+	}
+	glEnd();
+}
---
+++ lighting.c
@@ -0,0 +1,232 @@
+#include <GL/glut.h>
+#include <math.h>
+#include <string.h>
+#include <stdio.h>
+
+typedef float point3f[3];
+typedef struct {
+	int x;
+	int y;
+} pixel;
+
+const float AXIS_RADIUS = 5.0f;
+//const float MIN_FOV_ORTHO = 10.0f;
+const unsigned int EGG_SUBDIVISIONS = 20u;
+
+short viewmode = 0x1;
+short action_state = 0x0;
+
+pixel last_mouse_pos = (pixel){0, 0};
+pixel mouse_pos_delta = (pixel){0, 0};
+GLfloat degrees_per_pixel = 1.0f;
+
+GLdouble camera[] = {0.0, 0.0, 10.0};
+
+GLfloat model_rotation[] = {0.0f, 0.0f};
+
+GLfloat mat_ambient[] = {0.0f, 0.0f, 0.0f, 1.0f};
+GLfloat mat_diffuse[] = {0.0f, 0.0f, 0.0f, 1.0f};
+GLfloat mat_specular[] = {0.0f, 0.0f, 0.0f, 1.0f};
+GLfloat mat_shininess = 20.0f;
+
+#include "egg.c"
+
+void draw_axes() {
+	point3f x_axis_start = { -AXIS_RADIUS, 0.0f, 0.0f };
+	point3f x_axis_end = { AXIS_RADIUS, 0.0f, 0.0f };
+	point3f y_axis_start = { 0.0f, -AXIS_RADIUS, 0.0f };
+	point3f y_axis_end = { 0.0f, AXIS_RADIUS, 0.0f };
+	point3f z_axis_start = { 0.0f, 0.0f, -AXIS_RADIUS };
+	point3f z_axis_end = { 0.0f, 0.0f, AXIS_RADIUS };
+	
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glBegin(GL_LINES);
+	glVertex3fv(x_axis_start);
+	glVertex3fv(x_axis_end);
+	glEnd();
+	
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glBegin(GL_LINES);
+	glVertex3fv(y_axis_start);
+	glVertex3fv(y_axis_end);
+	glEnd();
+	
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glBegin(GL_LINES);
+	glVertex3fv(z_axis_start);
+	glVertex3fv(z_axis_end);
+	glEnd();
+}
+
+void render_scene() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	glLoadIdentity();
+	
+	gluLookAt(camera[0], camera[1], camera[2], 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+	
+	draw_axes();
+	
+	if (action_state & 1<<0) {
+		model_rotation[0] += mouse_pos_delta.x * degrees_per_pixel;
+		model_rotation[1] += mouse_pos_delta.y * degrees_per_pixel;
+		glRotatef(model_rotation[0], 0.0f, 1.0f, 0.0f);
+		glRotatef(model_rotation[1], 1.0f, 0.0f, 0.0f);
+	}
+	
+	if (viewmode & 1<<2)
+		draw_egg_faces();
+	if (viewmode & 1<<1)
+		draw_egg_edges();
+	if (viewmode & 1<<0)
+		draw_egg_vertices();
+	
+	if (!viewmode) {
+		GLfloat mat_ambient[] = {1.0f, 1.0f, 1.0f, 1.0f};
+		GLfloat mat_diffuse[] = {1.0f, 1.0f, 1.0f, 1.0f};
+		GLfloat mat_specular[] = {1.0f, 1.0f, 1.0f, 1.0f};
+		GLfloat mat_shininess = 20.0f;
+		
+		glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+		glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+		glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+		glMaterialf(GL_FRONT, GL_SHININESS, mat_shininess);
+		glutSolidTeapot(3.0);
+	}
+	
+	glFlush();
+	
+	glutSwapBuffers();
+}
+
+void init_render() {
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	
+	GLfloat light0_ambient[] = {0.1f, 0.1f, 0.1f, 1.0f};
+	GLfloat light0_diffuse[] = {1.0f, 1.0f, 1.0f, 1.0f};
+	GLfloat light0_specular[] = {1.0f, 1.0f, 1.0f, 1.0f};
+	GLfloat light0_position[] = {0.0f, 0.0f, 10.0f, 1.0f};
+	
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light0_ambient);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light0_diffuse);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light0_specular);
+	glLightfv(GL_LIGHT0, GL_POSITION, light0_position);
+	
+	glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 1.0f);
+	glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.05f);
+	glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, 0.001f);
+	
+	glShadeModel(GL_SMOOTH);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_NORMALIZE);
+}
+
+void resize_stage(GLsizei width, GLsizei height) {
+	if (height == 0)
+		height = 1;
+	
+	glViewport(0, 0, width, height);
+	
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	
+	gluPerspective(70.0, (GLdouble)width/(GLdouble)height, 1.0, 30.0);
+	
+//	GLfloat aspect_ratio = (GLfloat)width / (GLfloat)height;
+	
+/*	if (width <= height)
+		glOrtho(-MIN_FOV_ORTHO, MIN_FOV_ORTHO, -MIN_FOV_ORTHO/aspect_ratio, MIN_FOV_ORTHO/aspect_ratio, 128.0, -128.0);
+	else
+		glOrtho(-MIN_FOV_ORTHO*aspect_ratio, MIN_FOV_ORTHO*aspect_ratio, -MIN_FOV_ORTHO, MIN_FOV_ORTHO, 128.0, -128.0);
+*/	
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	
+	degrees_per_pixel = 360.0f / (GLfloat)width;
+}
+
+void key_pressed(unsigned char key, int x, int y) {
+	if (key == 'p')
+		viewmode ^= 1<<0;
+	else if (key == 'w')
+		viewmode ^= 1<<1;
+	else if (key == 's')
+		viewmode ^= 1<<2;
+	else if (key == '1') {
+		GLdouble new_camera[] = {0.0f, 0.0f, 10.0f};
+		memcpy(camera, new_camera, 3 * sizeof(GLdouble));
+	}
+	else if (key == '2') {
+		GLdouble new_camera[] = {15.0f, 15.0f, 15.0f};
+		memcpy(camera, new_camera, 3 * sizeof(GLdouble));
+	}
+	else if (key == '3') {
+		GLdouble new_camera[] = {0.0f, -15.0f, -15.0f};
+		memcpy(camera, new_camera, 3 * sizeof(GLdouble));
+	}
+	
+	render_scene();
+}
+
+void mouse_button_event(int button, int state, int x, int y) {
+	if (button == GLUT_LEFT_BUTTON) {
+		if (state == GLUT_DOWN) {
+			last_mouse_pos.x = x;
+			last_mouse_pos.y = y;
+			action_state |= 1<<0;
+		}
+		else {
+			action_state &= ~(1<<0);
+		}
+	}
+}
+
+void mouse_motion_event(int x, int y) {
+	if (action_state) {
+		mouse_pos_delta = (pixel){x - last_mouse_pos.x, y - last_mouse_pos.y};
+		last_mouse_pos = (pixel){x, y};
+		glutPostRedisplay();
+	}
+}
+
+char * get_gl_info() {
+	const char * gl_version = glGetString(GL_VERSION);
+	const char * gl_sl_version = glGetString(GL_SHADING_LANGUAGE_VERSION);
+	const char * gl_renderer = glGetString(GL_RENDERER);
+	
+	size_t gl_info_length = strlen("OpenGL  - GLSL  - ")
+				+ strlen(gl_version)
+				+ strlen(gl_sl_version)
+				+ strlen(gl_renderer);
+	
+	char * gl_info = malloc(gl_info_length + sizeof(char));
+	sprintf(gl_info, "OpenGL %s - GLSL %s - %s", gl_version, gl_sl_version, gl_renderer);
+	
+	return gl_info;
+}
+
+int main(int argc, char** argv) {
+	compute_egg_vertices();
+	
+	glutInit(&argc, argv);
+	
+	glutInitWindowSize(640, 480);
+	
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA | GLUT_DEPTH);
+	glutCreateWindow(NULL);
+	
+	glutSetWindowTitle(get_gl_info());
+	
+	glutDisplayFunc(render_scene);
+	glutReshapeFunc(resize_stage);
+	glutKeyboardFunc(key_pressed);
+	glutMouseFunc(mouse_button_event);
+	glutMotionFunc(mouse_motion_event);
+	
+	init_render();
+	
+	glutMainLoop();
+	return 0;
+}
